02e57caa865926f345146609043904b3fb2b9d83
vtbano <vtbano@gmail.com>
2022-06-17 15:35:15 -0400

    import BrowserRouter,Routes,Route into App.js. Add Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CreatorsLogin from "./CreatorsLogin";
 import Quizzes from "./Quizzes";
 import CreateQuiz from "./CreateQuiz";
@@ -27,81 +28,94 @@ const App = () => {
   const [imageUrl, setImageUrl] = useState("");
 
   return (
-    <React.Fragment>
-      <div className="page-body">
-        <header className="page-body-header">
-          <div></div>
-          <div className="title">
-            <span>BRUSH</span>
-            <span className="title-bold">UP</span>
-          </div>
-          <div className="navigation-options">
-            <NavigationButtons
-              setActiveContainer={setActiveContainer}
-              creator={creator}
-            />
-          </div>
-        </header>
-        <section className="page-body-main">
-          {/* if the text is true then it will return the second value. */}
-          {activeContainer === "Creators" && (
-            <CreatorsLogin setCreator={setCreator} />
-          )}
-          {activeContainer === "Quizzes" && (
-            <Quizzes
-              setActiveContainer={setActiveContainer}
-              creator={creator}
-              setActiveQuiz={setActiveQuiz}
-            />
-          )}
-          {activeContainer === "CreateQuiz" && (
-            <CreateQuiz
-              setActiveContainer={setActiveContainer}
-              creator={creator}
-              setActiveQuiz={setActiveQuiz}
-            />
-          )}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="register" element={<CreatorsLogin />} />
+          <Route path="quizzes" element={<Quizzes />} />
+          <Route path="quizzes/create" element={<CreateQuiz />} />
+          <Route path="quizzes/:id/edit" element={<EditQuizTitle />} />
+          <Route path="quizzes/:id/questions" element={<QuizQuestions />} />
+          <Route path="quizzes/:id/questions/add" element={<AddQuestion />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
 
-          {activeContainer === "EditQuizTitle" && (
-            <EditQuizTitle
-              setActiveContainer={setActiveContainer}
-              activeQuiz={activeQuiz}
-              setActiveQuiz={setActiveQuiz}
-            />
-          )}
-          {activeContainer === "QuizQuestions" && (
-            <QuizQuestions
-              setActiveContainer={setActiveContainer}
-              key={activeQuiz.id}
-              {...activeQuiz}
-              setActiveQuestion={setActiveQuestion}
-              setQuestionPlaceholder={setQuestionPlaceholder}
-              setImagePlaceholder={setImagePlaceholder}
-              setQuestionText={setQuestionText}
-              setImageUrl={setImageUrl}
-            />
-          )}
-          {activeContainer === "AddQuestion" && (
-            <AddQuestion
-              setActiveContainer={setActiveContainer}
-              key={activeQuestion.id}
-              {...activeQuestion}
-              questionPlaceholder={questionPlaceholder}
-              imagePlaceholder={imagePlaceholder}
-              setImagePlaceholder={setImagePlaceholder}
-              questionText={questionText}
-              setQuestionPlaceholder={setQuestionPlaceholder}
-              setQuestionText={setQuestionText}
-              imageUrl={imageUrl}
-              setImageUrl={setImageUrl}
-            />
-          )}
-        </section>
-        <footer className="page-body-footer">
-          <Footer />
-        </footer>
-      </div>
-    </React.Fragment>
+    // <React.Fragment>
+    //   <div className="page-body">
+    //     <header className="page-body-header">
+    //       <div></div>
+    //       <div className="title">
+    //         <span>BRUSH</span>
+    //         <span className="title-bold">UP</span>
+    //       </div>
+    //       <div className="navigation-options">
+    //         <NavigationButtons
+    //           setActiveContainer={setActiveContainer}
+    //           creator={creator}
+    //         />
+    //       </div>
+    //     </header>
+    //     <section className="page-body-main">
+    //       {/* if the text is true then it will return the second value. */}
+    //       {activeContainer === "Creators" && (
+    //         <CreatorsLogin setCreator={setCreator} />
+    //       )}
+    //       {activeContainer === "Quizzes" && (
+    //         <Quizzes
+    //           setActiveContainer={setActiveContainer}
+    //           creator={creator}
+    //           setActiveQuiz={setActiveQuiz}
+    //         />
+    //       )}
+    //       {activeContainer === "CreateQuiz" && (
+    //         <CreateQuiz
+    //           setActiveContainer={setActiveContainer}
+    //           creator={creator}
+    //           setActiveQuiz={setActiveQuiz}
+    //         />
+    //       )}
+
+    //       {activeContainer === "EditQuizTitle" && (
+    //         <EditQuizTitle
+    //           setActiveContainer={setActiveContainer}
+    //           activeQuiz={activeQuiz}
+    //           setActiveQuiz={setActiveQuiz}
+    //         />
+    //       )}
+    //       {activeContainer === "QuizQuestions" && (
+    //         <QuizQuestions
+    //           setActiveContainer={setActiveContainer}
+    //           key={activeQuiz.id}
+    //           {...activeQuiz}
+    //           setActiveQuestion={setActiveQuestion}
+    //           setQuestionPlaceholder={setQuestionPlaceholder}
+    //           setImagePlaceholder={setImagePlaceholder}
+    //           setQuestionText={setQuestionText}
+    //           setImageUrl={setImageUrl}
+    //         />
+    //       )}
+    //       {activeContainer === "AddQuestion" && (
+    //         <AddQuestion
+    //           setActiveContainer={setActiveContainer}
+    //           key={activeQuestion.id}
+    //           {...activeQuestion}
+    //           questionPlaceholder={questionPlaceholder}
+    //           imagePlaceholder={imagePlaceholder}
+    //           setImagePlaceholder={setImagePlaceholder}
+    //           questionText={questionText}
+    //           setQuestionPlaceholder={setQuestionPlaceholder}
+    //           setQuestionText={setQuestionText}
+    //           imageUrl={imageUrl}
+    //           setImageUrl={setImageUrl}
+    //         />
+    //       )}
+    //     </section>
+    //     <footer className="page-body-footer">
+    //       <Footer />
+    //     </footer>
+    //   </div>
+    // </React.Fragment>
   );
 };
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <Routes>
-      {/* <Route path="/" element={<App />}>
-        <Route index element={<Home />} />
-        <Route path="teams" element={<Teams />}>
-          <Route path=":teamId" element={<Team />} />
-          <Route path="new" element={<NewTeamForm />} />
-          <Route index element={<LeagueStandings />} />
-        </Route>
-      </Route> */}
-    </Routes>
-  </BrowserRouter>
-);
