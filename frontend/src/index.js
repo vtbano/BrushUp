9ed9754d3930f162aef0adf49efe841f7ccfdf9e
vtbano <vtbano@gmail.com>
2022-06-16 23:04:37 -0400

    install react-router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      {/* <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="teams" element={<Teams />}>
+          <Route path=":teamId" element={<Team />} />
+          <Route path="new" element={<NewTeamForm />} />
+          <Route index element={<LeagueStandings />} />
+        </Route>
+      </Route> */}
+    </Routes>
+  </BrowserRouter>
+);
