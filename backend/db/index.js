51e26f20e929cb540eb7ec352676bbb76c6df238
vtbano <vtbano@gmail.com>
2022-07-15 20:53:58 -0400

    add ternary operator to index.js

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,19 +1,19 @@
 const { Pool } = require("pg");
-const {
-  user,
-  host,
-  database,
-  password,
-  port,
-} = require("../secrets/db_configuration");
 
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 // const pool = new Pool({ user, host, database, password, port });
-const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-});
+const pool = isProduction
+  ? new Pool({
+      connectionString,
+      ssl: { rejectUnauthorized: false },
+    })
+  : new Pool({
+      user: process.env.PG_USER,
+      host: process.env.PG_HOST,
+      database: process.env.PG_DATABASE,
+      password: process.env.PG_PASSWORD,
+      port: process.env.PG_PORT,
+    });
 
 module.exports = pool;
