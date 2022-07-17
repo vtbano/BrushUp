0a57b0b1695fdededc68249efac3617e8ebb92a0
vtbano <vtbano@gmail.com>
2022-07-16 22:00:06 -0400

    delete commented out code

--- backend/db/index.js
+++ backend/db/index.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const isProduction = process.env.NODE_ENV === "production";
 const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
-// const pool = new Pool({ user, host, database, password, port });
+
 const pool = isProduction
   ? new Pool({
       connectionString,
