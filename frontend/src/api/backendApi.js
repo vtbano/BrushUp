0d09d00078c9d0c1bbf2fc1b9740ae0347dc8dbd
vtbano <vtbano@gmail.com>
2022-07-17 11:28:33 -0400

    update baseUrl

--- frontend/src/api/backendApi.js
+++ frontend/src/api/backendApi.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "http://protected-retreat-11747.herokuapp.com"
+    ? "https://protected-retreat-11747.herokuapp.com"
     : // ? "https://git.heroku.com/protected-retreat-11747.git/brushup"
       "http://localhost:3001";
 
