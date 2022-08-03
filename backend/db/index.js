eaba7fa980ed4f7b6632fb1680a7ecb30f464720
vtbano <vtbano@gmail.com>
2022-08-03 17:57:56 -0400

    update index.js

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const isProduction = process.env.NODE_ENV === "production";
-const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+// const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const pool = isProduction
   ? new Pool({
-      connectionString,
+      connectionString: process.env.DATABASE_URL,
       ssl: { rejectUnauthorized: false },
     })
   : new Pool({
